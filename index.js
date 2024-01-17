348631f62e0d0aa2a7f40a2f33b8253860c23b10
radtradcath <vinicius.b.bonafini@gmail.com>
2024-01-17 10:08:10 -0300

    Refactor to use Express

--- index.js
+++ index.js
@@ -1,23 +1,52 @@
-const http = require('http');
-const url = require('url');
-const fs = require('fs')
+const express = require('express')
 
-http.createServer((req, res) => {
-    const q = url.parse(req.url, true);
-    const page = q.pathname === "/" ? './index.html' : `.${q.pathname}`
-   
-    fs.readFile(page, (err, data) => {
-        if (err) {
-            res.writeHead(404, {"Content-Type": "text/html"})
-            fs.readFile('404.html', (err, data) => {
-                if (err) throw err;
-                res.write(data)
-                return;
-            })
-            return;
-        }
-        res.writeHead(200, {"Content-Type": "text/html"})
-        res.write(data)
-        return res.end()
+const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile('/home/radtrad/repos/node-informational-site/index.html', (err) => {
+        if (err) throw err;
     })
-}).listen(8080)
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile('/home/radtrad/repos/node-informational-site/about.html', (err) => {
+        if (err) throw err;
+    })
+})
+
+app.get('/contact-me', (req, res) => {
+    res.sendFile('/home/radtrad/repos/node-informational-site/contact-me.html', (err) => {
+        if (err) throw err;
+    })
+})
+
+app.use((req, res, next) => {
+    res.status(404).sendFile('/home/radtrad/repos/node-informational-site/404.html', (err) => {
+        if (err) throw err;
+    })
+})
+
+app.listen(3000, () => {
+    console.log(`Listening at http://localhost:8000`)
+})
+
+// http.createServer((req, res) => {
+//     const q = url.parse(req.url, true);
+//     const page = q.pathname === "/" ? './index.html' : `.${q.pathname}`
+   
+//     fs.readFile(page, (err, data) => {
+//         if (err) {
+//             res.writeHead(404, {"Content-Type": "text/html"})
+//             fs.readFile('404.html', (err, data) => {
+//                 if (err) throw err;
+//                 res.write(data)
+//                 return;
+//             })
+//             return;
+//         }
+//         res.writeHead(200, {"Content-Type": "text/html"})
+//         res.write(data)
+//         return res.end()
+//     })
+// }).listen(8080)
+
